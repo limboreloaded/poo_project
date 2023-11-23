39039a7b07668a5ca459077622751e373e6d1dee
BTAndrew <97854217+BTAndrew@users.noreply.github.com>
2023-11-23 20:31:25 +0200

    Added getters and setters for Person class
    Added getters and setters for each private attribute.

--- person.cpp
+++ person.cpp
@@ -10,7 +10,54 @@ class Person {
             this->balance = 0.0;
             this->hasDiscount = false;
             strcpy(this->phone, "0000000000");
+            this->attendedEvents = nullptr;
         }
+
+        Person(string name, int age, double balance, const char* phone) {
+            this->name = name;
+            this->age = age;
+            this->balance = balance;
+            this->hasDiscount = age < 18 || age > 60 ? true : false;
+            strcpy(this->phone, phone);
+        }
+
+        string getName() {
+            return this->name;
+        }
+
+        void setName(string name) {
+            this->name = name;
+        }
+
+        double getBalance() {
+            return this->balance;
+        }
+
+        void setBalance(double balance) {
+            this->balance = balance;
+        }
+
+        bool getHasDiscount() {
+            return this->hasDiscount;
+        }
+
+        void setHasDiscount(bool hasDiscount) {
+            this->hasDiscount = hasDiscount;
+        }
+
+        char* getPhone() {
+            return this->phone;
+        }
+
+        char* setPhone(char* phone) {
+            strcpy(this->phone, phone);
+        }
+
+        ~Person() {
+            if (this->attendedEvents != nullptr) {
+                delete[] this->attendedEvents;
+            }
+        } 
 
     private:
         string name;
@@ -18,6 +65,5 @@ class Person {
         double balance;
         bool hasDiscount;
         char phone[10];
-
-        //! Ticket boughtTickets;
+        string* attendedEvents;
 };
